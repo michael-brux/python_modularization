5296486ee388a3940179264ef02f1bc284aaba37
Michael Brux <7270100+michael-brux@users.noreply.github.com>
2025-02-26 21:10:27 +0100

    Update hello_script.py

--- hello_script.py
+++ hello_script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Hello Script"""
+""" Hello Script """
 from pathlib import Path
 from hello_utils import print_module_info
 from hello_module import get_greet, get_filetime
@@ -7,11 +7,6 @@ from hello_module import get_greet, get_filetime
 print("\nHello, World! - from script")
 
 print_module_info(__file__, __name__)
-#print()
-#print("filename of __file__ is      :", Path(__file__).name)
-#print("__name__ is                  :", __name__)
-#print("current working directory is :", Path.cwd())
-#print("directory of __file__ is     :", Path(__file__).parent)
 
 print()
 print("with help of imported module :", get_greet())
